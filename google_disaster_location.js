fdec2adfe30522b7f0eea4b69d5fd78839d7672c
Kesh <mail45@inbox.ru>
2011-06-14 17:32:38 +0600

    Issue bugs fixed  by korgx9: litle bugs fixed.

--- google_disaster_location.js
+++ google_disaster_location.js
@@ -27,12 +27,15 @@ function initialize() {
     map.setCenter(new google.maps.LatLng(Drupal.settings.map_config.Lat, Drupal.settings.map_config.Lon));
     map.setZoom(10);
   }
+  else if(Drupal.settings.earthquakes_data[0].Lat != 'undefined') {
+    map.setCenter(new google.maps.LatLng(Drupal.settings.earthquakes_data[0].Lat, Drupal.settings.earthquakes_data[0].Lon));
+    map.setZoom(parseInt(Drupal.settings.map_config.zoom));//Get center to the latest disaster ONLY eargtquakes TODO for all
+  }
   else {
-    map.setCenter(new google.maps.LatLng(Drupal.settings.map_config.Lat, Drupal.settings.map_config.Lon));
+    map.setCenter(new google.maps.LatLng(0, 0)); //Nothing to show map cenetr gets param (0, 0) 
     map.setZoom(parseInt(Drupal.settings.map_config.zoom));
   }
 
-//  var host = (window.location + "").slice(0,(window.location + "").indexOf("google_disaster_location"));
   var path_to_module = Drupal.settings.path_to_module;
 
   if(Drupal.settings.earthquakes_data != null) {
